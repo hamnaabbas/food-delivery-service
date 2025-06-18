d50ac0b276ffbbcd4a73862be43a9503c62a7d32
Hamna abbas <112249808+hamnaabbas@users.noreply.github.com>
2025-06-18 02:27:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/food-delivery-service">
-  <App />
-</BrowserRouter>
-
+         <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
